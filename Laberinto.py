cc33abd81443818df1c6e1db1b1d069ae9e18b95
Erincam09 <159506921+Erincam09@users.noreply.github.com>
2025-04-15 13:36:44 -0600

    Cambios

--- Laberinto.py
+++ Laberinto.py
@@ -13,43 +13,69 @@ def crear_Matriz(tamano):
     return matriz
 
 
-def mostrar_matriz(matriz):
+def mostrar_matriz(matriz, nodoInicio, nodoFinal):
+    if nodoInicio != None:
+        matriz[nodoInicio[0]][nodoInicio[1]] = 2
+    if nodoFinal != None:
+        matriz[nodoFinal[0]][nodoFinal[1]] = 3
     for fila in matriz:
-        print(" ".join("🟩" if num == 1 else "🚧" for num in fila))  # 1=🟩, 0=🚧
+        print(fila)
 
-# prueba = crear_Matriz(25)
-# mostrar_matriz(prueba)
+def nodo_final(matriz):
+    tam = len(matriz)
+    posibles = []
+    for i in range(tam):
+        for j in range(tam):
+            if matriz[i][j] == 1:
+                posibles.append([i, j])
+    if posibles != None:
+        return random.choice(posibles)
 
-def backtracking(matriz, nodoInicio, nodoFinal):
-    matriz[nodoInicio[0]][nodoInicio[1]] = 2
-    matriz[nodoFinal[0]][nodoFinal[1]] = 3
+def backtracking(matriz, nodoInicio):
     listaCaminos = []
-    busqueda(matriz, None, nodoInicio, [], listaCaminos)
-    print(listaCaminos)
+    visitados = []
+    busqueda(matriz, None, nodoInicio, [], listaCaminos, visitados)
+    return listaCaminos
 
-def busqueda(matriz, nodoAnterior, nodoActual, lista, listaCaminos):
+def busqueda(matriz, nodoAnterior, nodoActual, lista, listaCaminos, visitados):
     if(nodoActual[0] >= 0 and nodoActual[1] >= 0 and nodoActual[0] < len(matriz) and nodoActual[1] < len(matriz)):
+        for elem in visitados:
+            if elem == nodoActual:
+                return
         if (matriz[nodoActual[0]][nodoActual[1]] == 3):
             listaCaminos += [lista + [nodoActual]]
         if (matriz[nodoActual[0]][nodoActual[1]] == 2 or matriz[nodoActual[0]][nodoActual[1]] == 1):
+            visitados += [nodoActual]
             # Arriba
             if (nodoAnterior != [nodoActual[0]-1, nodoActual[1]]):
-                busqueda(matriz, nodoActual, [nodoActual[0]-1, nodoActual[1]], lista + [nodoActual], listaCaminos)
+                busqueda(matriz, nodoActual, [nodoActual[0]-1, nodoActual[1]], lista + [nodoActual], listaCaminos, visitados)
             # Derecha
             if (nodoAnterior != [nodoActual[0], nodoActual[1]+1]):
-                busqueda(matriz, nodoActual, [nodoActual[0], nodoActual[1]+1], lista + [nodoActual], listaCaminos)
+                busqueda(matriz, nodoActual, [nodoActual[0], nodoActual[1]+1], lista + [nodoActual], listaCaminos, visitados)
             # Abajo
             if (nodoAnterior != [nodoActual[0]+1, nodoActual[1]]):
-                busqueda(matriz, nodoActual, [nodoActual[0]+1, nodoActual[1]], lista + [nodoActual], listaCaminos)
+                busqueda(matriz, nodoActual, [nodoActual[0]+1, nodoActual[1]], lista + [nodoActual], listaCaminos, visitados)
             # Izquierda
             if (nodoAnterior != [nodoActual[0], nodoActual[1]-1]):
-                busqueda(matriz, nodoActual, [nodoActual[0], nodoActual[1]-1], lista + [nodoActual], listaCaminos)
-        
+                busqueda(matriz, nodoActual, [nodoActual[0], nodoActual[1]-1], lista + [nodoActual], listaCaminos, visitados)
 
-matriz = [[1,0,1,1,0],
-          [1,1,0,1,0],
-          [0,1,1,1,1],
-          [0,0,1,0,1],
-          [0,1,1,1,1]]
+            visitados.pop()
 
-backtracking(matriz, [0,0], [3,4])
+def mejorCamino(lista):
+    mejor = lista[0]
+    for camino in lista:
+        if len(camino)<len(mejor):
+            mejor = camino
+    return mejor
+
+prueba = crear_Matriz(5)
+final = nodo_final(prueba)
+mostrar_matriz(prueba, None, final)
+fil = int(input("Numero de fila:\n"))
+col = int(input("Numero de columna:\n"))
+mostrar_matriz(prueba, [fil,col], None)
+print("\n")
+caminos = backtracking(prueba, [fil,col])
+CaminoOptimo = mejorCamino(caminos)
+print(caminos)
+print(CaminoOptimo)
