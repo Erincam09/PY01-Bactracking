5dc7b6415be5f28719772f97dda85343bd887f29
Ginger Rodriguez Gutierrez <girodriguez@estudiantec.cr>
2025-04-21 13:42:47 -0600

    base de interfaz con generacion de laberinto

--- Laberinto.py
+++ Laberinto.py
@@ -7,8 +7,11 @@ class InterfazLaberinto:
         self.root = root
         self.root.title("Laberinto")
         self.root.geometry("800x600")
-
-        self.crear_controles() 
+        self.matriz = []
+        self.caminos = []
+        self.camino_actual = 0
+        self.crear_controles()
+        self.crear_laberinto_frame() 
 
     def crear_controles(self):
         """ 
@@ -27,8 +30,8 @@ class InterfazLaberinto:
         btn_resolver.pack(side=tk.LEFT, padx=5)
 
         #Seleccion de tamaño de laberinto, configuraciones
-        label_tamaño = ttk.Label(frame_controles, text="Tamaño: ")
-        label_tamaño.pack(side=tk.LEFT, padx=5)
+        label_tamano = ttk.Label(frame_controles, text="Tamaño: ")
+        label_tamano.pack(side=tk.LEFT, padx=5)
 
         self.spin_tamano = ttk.Spinbox(frame_controles, from_=5, to=20, width=5) #un spinbox que vaya de 5x5 a 25x25 | Implementar
         self.spin_tamano.pack(side=tk.LEFT, padx=5)
@@ -37,27 +40,149 @@ class InterfazLaberinto:
         self.frame_laberinto =ttk.Frame(self.root)
         self.frame_laberinto.pack(expand=True, fill=tk.BOTH)
 
+        #Boton camino siguiente
+        self.btn_siguiente = ttk.Button(frame_controles, text= "Siguiente camino", command=self.mostrar_siguiente_camino,
+                                        state=tk.DISABLED)
+        self.btn_siguiente.pack(side=tk.LEFT, padx=5)
+
+        # Mensajes 
+        self.mensaje_var = tk.StringVar()
+        self.mensaje_var.set("Listo para generar laberinto")
+        self.label_mensaje = ttk.Label(frame_controles, textvariable=self.mensaje_var,
+                                       relief=tk.SUNKEN,padding=(5,2))
+        self.label_mensaje.pack(side=tk.RIGHT, fill=tk.X, expand=True)
+        self.label_mensaje.config(
+            font=('Arial', 10),
+            anchor=tk.W,  # Alinear texto a la izquierda
+            wraplength=300  # Permite múltiples líneas
+        )
+
+
+    def crear_laberinto_frame(self):
+        "Frame para dibujar el labertinto"
+        self.frame_laberinto = ttk.Frame(self.root)
+        self.frame_laberinto.pack(expand=True,fill=tk.BOTH, padx=10, pady=10)
+
+
     def generar_laberinto(self):
-        print("Generando Bakcroom")
+        try:
+            tamano = int(self.spin_tamano.get())
+            self.matriz = crear_Matriz(tamano)
+            for widget in self.frame_laberinto.winfo_children():
+                widget.destroy()
+
+            self.dibujar_matriz()
+
+            self.caminos = []
+            self.camino_actual = 0
+            self.btn_siguiente.config(state=tk.DISABLED)
+            self.mostrar_mensaje("Laberinto  generado con exito", 'exito')
+        except ValueError:
+            self.mostrar_mensaje("Error: Por favor ingrese un tamaño de matriz válido (5x5 - 25x25)", 'error')
+
+
+    def dibujar_matriz(self, camino=None):
+        "Dibuja la matriuz en el frame_laberinto"
+        for i in range(len(self.matriz)):
+            for j in range(len(self.matriz[i])):
+                valor = self.matriz[i][j]
+                if valor == 0:
+                    bg_color = "gray20"
+                    text = ""
+                elif valor == 1:
+                    bg_color = "white"
+                    text=""
+                elif valor == 2: 
+                    bg_color = "blue"
+                    text="I"
+                elif valor == 3:
+                    bg_color = "red"
+                    text="F"
+                if camino and [i,j] in camino:
+                    bg_color = "green"
+                    text="•"
+
+                celda = tk.Label(self.frame_laberinto, text=text, bg=bg_color,
+                                 width=3, height=1, relief="ridge",font=('Arial', 10))
+                celda.grid(row=i, column=j, padx=1, pady=1)
+
+    
 
     def resolver_laberinto(self):
-        print("Buscando ruta de escape")
+        "Encontrar todos los posibles caminos usando el algoritmo de Backtracking"
+        if not self.matriz:
+            self.mostrar_mensaje("Primero genera un laberinto", 'error')
+            return
+        
+        
 
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = InterfazLaberinto(root)
-    root.mainloop()
+        #Seleccionar Posicion
+        tamano = len(self.matriz)
+        inicio = [0,0]
+        fin = [tamano-1, tamano-1]
+
+        #validar error en posicion con respecto a tamaño de matriz
+        if self.matriz[inicio[0]][inicio[1]] != 1:
+            self.mostrar_mensaje("Posición inicial no es válida", 'error')
+            return
+        
+        if self.matriz[fin[0]][fin[1]] != 1:
+            self.mostrar_mensaje("Posición final no es válida | Genere el laberinto nuevamente", 'error')
+            return
+
+        self.matriz[inicio[0]][inicio[1]] = 2 #Inicio
+        self.matriz[fin[0]][fin[1]] = 3 #final
+        self.dibujar_matriz()
+
+        self.caminos = backtracking(self.matriz, inicio)
+
+        if self.caminos:
+            self.mostrar_mensaje("Se encontraron " + str(len(self.caminos)) + " caminos", 'exito')
+            self.camino_actual = 0
+            self.dibujar_matriz(self.caminos[self.camino_actual])
+            self.btn_siguiente.config(state=tk.NORMAL)
+
+        else:
+            self.mostrar_mensaje("No se encontraron caminos", 'error')
+            self.dibujar_matriz()
+
+    
+    def mostrar_siguiente_camino(self):
+        if self.caminos:
+            self.camino_actual = (self.camino_actual + 1) % len(self.caminos)
+            self.dibujar_matriz(self.caminos[self.camino_actual])
+
+
+    def mostrar_mensaje(self, mensaje, tipo='info'):
+        colores = {
+            'info': 'black',
+            'error': 'red',
+            'exito': 'green',
+            'debug': 'blue'
+        }
+        self.mensaje_var.set(mensaje)
+        self.label_mensaje.config(foreground=colores.get(tipo, 'black'),
+                                  font=('Arial', 10, 'italic' if tipo == 'debug' else 'normal'))
+        self.root.update_idletasks()
+
+#CAMBIO ------------------------------------------------------
+
+"""
+crear matriz dinamica, asegurando al menos un camino a punto b dejando las esquinas libres
 """
 def crear_Matriz(tamano):
     matriz = []
     for fila in range(tamano):
         nueva_fila = []
         for columna in range(tamano):
-            if random.random() < 0.7:
+            if (fila == 0 and columna == 0) or (fila == tamano-1 and columna == tamano-1):
                 nueva_fila.append(1)
-            else:
-                nueva_fila.append(0)
+            else:  
+                if random.random() < 0.7:
+                    nueva_fila.append(1)
+                else:
+                    nueva_fila.append(0)
         matriz.append(nueva_fila)
     return matriz
 
@@ -120,6 +245,12 @@ def mejorCamino(lista):
             mejor = camino
     return mejor
 
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = InterfazLaberinto(root)
+    root.mainloop()
+"""
 prueba = crear_Matriz(5)
 tam = len(prueba)
 final = nodo_final(prueba)
