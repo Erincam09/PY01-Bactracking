1e1cb3371247ccc1b4366949c553d4e909dc5559
Ginger Rodriguez Gutierrez <girodriguez@estudiantec.cr>
2025-04-21 16:41:45 -0600

    caminos con colores

--- Laberinto.py
+++ Laberinto.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import random
+from collections import deque
 
 class InterfazLaberinto:
     def __init__(self, root):
@@ -10,8 +11,9 @@ class InterfazLaberinto:
         self.matriz = []
         self.caminos = []
         self.camino_actual = 0
-        self.crear_controles()
         self.crear_laberinto_frame() 
+        self.caminos_especiales = {}
+        self.crear_controles()
 
     def crear_controles(self):
         """ 
@@ -57,6 +59,18 @@ class InterfazLaberinto:
             wraplength=300  # Permite múltiples líneas
         )
 
+        frame_caminos = ttk.Frame(frame_controles)
+        frame_caminos.pack(side=tk.LEFT, padx=10, pady=5)
+
+        btn_corto = ttk.Button(frame_caminos, text="Camino corto", command=lambda: self.mostrar_camino_especial('corto'))
+        btn_corto.pack(side=tk.LEFT, padx=2)
+
+        btn_optimo = ttk.Button(frame_caminos, text="Camino óptimo", command=lambda: self.mostrar_camino_especial('optimo'))
+        btn_optimo.pack(side=tk.LEFT, padx=2)
+
+        btn_largo = ttk.Button(frame_caminos, text="Camino largo", command=lambda: self.mostrar_camino_especial('largo'))
+        btn_largo.pack(side=tk.LEFT, padx=2)
+
 
     def crear_laberinto_frame(self):
         "Frame para dibujar el labertinto"
@@ -68,6 +82,12 @@ class InterfazLaberinto:
         try:
             tamano = int(self.spin_tamano.get())
             self.matriz = crear_Matriz(tamano)
+
+            #Asegurar que las esquinas sean caminos
+            self.matriz[0][0] = 1
+            self.matriz[tamano-1][tamano-1] = 1
+
+            #Limbiar y dibujar consola
             for widget in self.frame_laberinto.winfo_children():
                 widget.destroy()
 
@@ -82,29 +102,11 @@ class InterfazLaberinto:
 
 
     def dibujar_matriz(self, camino=None):
-        "Dibuja la matriuz en el frame_laberinto"
-        for i in range(len(self.matriz)):
-            for j in range(len(self.matriz[i])):
-                valor = self.matriz[i][j]
-                if valor == 0:
-                    bg_color = "gray20"
-                    text = ""
-                elif valor == 1:
-                    bg_color = "white"
-                    text=""
-                elif valor == 2: 
-                    bg_color = "blue"
-                    text="I"
-                elif valor == 3:
-                    bg_color = "red"
-                    text="F"
-                if camino and [i,j] in camino:
-                    bg_color = "green"
-                    text="•"
-
-                celda = tk.Label(self.frame_laberinto, text=text, bg=bg_color,
-                                 width=3, height=1, relief="ridge",font=('Arial', 10))
-                celda.grid(row=i, column=j, padx=1, pady=1)
+        if camino is not None:
+            color_camino = 'green'
+            self.dibujar_matriz_especial(camino, color_camino)
+        else:
+            self.dibujar_matriz_especial([],None)
 
     
 
@@ -114,7 +116,7 @@ class InterfazLaberinto:
             self.mostrar_mensaje("Primero genera un laberinto", 'error')
             return
         
-        
+        self.matriz_original = [fila[:] for fila in self.matriz]
 
 
         #Seleccionar Posicion
@@ -125,11 +127,15 @@ class InterfazLaberinto:
         #validar error en posicion con respecto a tamaño de matriz
         if self.matriz[inicio[0]][inicio[1]] != 1:
             self.mostrar_mensaje("Posición inicial no es válida", 'error')
-            return
+            # Forzar posición inicial válida
+            self.matriz[inicio[0]][inicio[1]] = 1
+            self.matriz_original[inicio[0]][inicio[1]] = 1
         
         if self.matriz[fin[0]][fin[1]] != 1:
-            self.mostrar_mensaje("Posición final no es válida | Genere el laberinto nuevamente", 'error')
-            return
+            self.mostrar_mensaje("Posición final no es válida", 'error')
+            # Forzar posición final válida
+            self.matriz[fin[0]][fin[1]] = 1
+            self.matriz_original[fin[0]][fin[1]] = 1
 
         self.matriz[inicio[0]][inicio[1]] = 2 #Inicio
         self.matriz[fin[0]][fin[1]] = 3 #final
@@ -138,6 +144,7 @@ class InterfazLaberinto:
         self.caminos = backtracking(self.matriz, inicio)
 
         if self.caminos:
+            self.encontrar_caminos_especiales()
             self.mostrar_mensaje("Se encontraron " + str(len(self.caminos)) + " caminos", 'exito')
             self.camino_actual = 0
             self.dibujar_matriz(self.caminos[self.camino_actual])
@@ -145,13 +152,107 @@ class InterfazLaberinto:
 
         else:
             self.mostrar_mensaje("No se encontraron caminos", 'error')
+            self.matriz = [fila[:] for fila in self.matriz_original]
             self.dibujar_matriz()
 
+
+#Encuentra los caminos mas corto, largo y optimo
+    def encontrar_caminos_especiales(self):
+        if not self.caminos:
+            return
+        self.caminos_especiales['corto'] = min(self.caminos, key=len)
+        self.caminos_especiales['largo'] = max(self.caminos, key=len)
+        self.caminos_especiales['optimo'] = self.encontrar_camino_optimo()
+
+
+    def encontrar_camino_optimo(self):
+        #Primero ordenamos por longitud
+        caminos_ordenados = sorted(self.caminos, key=len)
+        #Elegimos el 25% de los mas cortos
+        mejores_caminos = caminos_ordenados[:max(1, len(caminos_ordenados)//4)]
+
+        return min(mejores_caminos, key=self.calcular_cambios_direccion)
     
+
+    def calcular_cambios_direccion(self,camino):
+        cambios = 0
+        if len(camino) < 2:
+            return 0
+        direccion_anterior = (
+            camino[1][0] - camino[0][0],
+            camino[1][1] - camino[0][1]
+        )
+
+        for i in range(2, len(camino)):
+            direccion_actual = (
+                camino[i][0] - camino[i-1][0],
+                camino[i][1] - camino[i-1][1]
+            )
+            if direccion_actual != direccion_anterior:
+                cambios += 1
+                direccion_anterior = direccion_actual
+        return cambios
+
+    def mostrar_camino_especial(self,tipo):
+        if not self.caminos or tipo not in self.caminos_especiales:
+            return
+        
+        #resetear matriz
+        self.matriz = [fila[:] for fila in self.matriz_original]
+
+        tamano = len(self.matriz)
+        self.matriz[0][0] = 2
+        self.matriz[tamano-1][tamano-1] = 3
+
+        color = {
+            'corto': 'green',
+            'optimo': 'yellow',
+            'largo': 'orange'
+        }.get(tipo, 'green')
+        self.dibujar_matriz_especial(self.caminos_especiales[tipo], color)
+        self.mostrar_mensaje("Mostrando camino "+ tipo, 'info')
+
+
+    def dibujar_matriz_especial(self, camino, color_camino):
+        """Dibuja la matriz resaltanmdp un camino con color especial"""
+        for i in range(len(self.matriz)):
+            for j in range(len(self.matriz[i])):
+                valor = self.matriz[i][j]
+
+                if valor == 0:
+                    bg_color = "gray20"
+                    text = ""
+                elif valor == 1:
+                    bg_color = "white"
+                    text = ""
+                elif valor == 2:
+                    bg_color = "blue"
+                    text = "I"
+                elif valor == 3:
+                    bg_color = "red"
+                    text = "F"
+                if [i,j] in camino:
+                    bg_color = color_camino
+                    text = "•"
+                celda = tk.Label(self.frame_laberinto, text=text, bg=bg_color, width=3,
+                                 height=1, relief="ridge", font=('Arial',10))
+                celda.grid(row=i, column=j, padx=1, pady=1)
+    
+
     def mostrar_siguiente_camino(self):
-        if self.caminos:
-            self.camino_actual = (self.camino_actual + 1) % len(self.caminos)
-            self.dibujar_matriz(self.caminos[self.camino_actual])
+        if not self.caminos:
+            return
+        
+        self.matriz = [fila[:] for fila in self.matriz_original]
+
+        #Volver a marcar inicio y fin
+        tamano = len(self.matriz)
+        self.matriz[0][0] = 2
+        self.matriz[tamano-1][tamano-1] = 3
+
+        #Siguiente Camino
+        self.camino_actual = (self.camino_actual + 1) % len(self.caminos)
+        self.dibujar_matriz_especial(self.caminos[self.camino_actual], 'green')
 
 
     def mostrar_mensaje(self, mensaje, tipo='info'):
@@ -168,24 +269,54 @@ class InterfazLaberinto:
 
 #CAMBIO ------------------------------------------------------
 
-"""
-crear matriz dinamica, asegurando al menos un camino a punto b dejando las esquinas libres
-"""
-def crear_Matriz(tamano):
-    matriz = []
-    for fila in range(tamano):
-        nueva_fila = []
-        for columna in range(tamano):
-            if (fila == 0 and columna == 0) or (fila == tamano-1 and columna == tamano-1):
-                nueva_fila.append(1)
-            else:  
-                if random.random() < 0.7:
-                    nueva_fila.append(1)
-                else:
-                    nueva_fila.append(0)
-        matriz.append(nueva_fila)
-    return matriz
 
+def camino_valido(matriz):
+    if not matriz or matriz[0][0] == 0 or matriz[-1][-1] == 0:
+        return False
+
+    tamano = len(matriz)
+    visitados = [[False for _ in range(tamano)] for _ in range(tamano)]
+
+    def dfs(x,y):
+        if x == tamano-1 and y == tamano-1:
+            return True
+        
+        visitados[x][y] = True
+        direcciones = [(0,1),(1,0),(0,-1),(-1,0)]
+
+        for direccion in direcciones:
+            dx, dy = direccion
+            nuevo_x = x + dx
+            nuevo_y = y + dy
+            
+            coordenada_valida = (0 <= nuevo_x < tamano and 0 <= nuevo_y < tamano)
+            if coordenada_valida and not visitados[nuevo_x][nuevo_y] and matriz[nuevo_x][nuevo_y] == 1:
+                if dfs(nuevo_x, nuevo_y):
+                    return True
+        return False
+    return dfs(0,0) 
+
+
+def crear_Matriz(tamano):
+    """
+    crear matriz dinamica, asegurando al menos un camino a punto b dejando las esquinas libres
+    """
+    intentos_maximos = 10
+    for _ in range(intentos_maximos):
+        matriz_temp = []
+        for fila in range (tamano):
+            fila_temp = []
+            for columna in range(tamano):
+                if (fila == 0 and columna == 0) or (fila == tamano-1 and columna == tamano-1):
+                    fila_temp.append(1)
+                else:
+                    fila_temp.append(1 if random.random() < 0.7 else 0)
+            matriz_temp.append(fila_temp)
+        if camino_valido(matriz_temp):
+            return matriz_temp
+        
+    #Generar laberrinto abierto despues de muchos intentos para que no explote
+    return [[1 for _ in range(tamano)] for _ in range(tamano)]
 
 def mostrar_matriz(matriz, nodoInicio, nodoFinal):
     if nodoInicio != None:
